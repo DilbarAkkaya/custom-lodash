b97859c38ca2ba23cb0a626a453ece0539dad8a7
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-02-23 18:41:26 +0300

    refactor: replace custom-lodash folder from pulic repo

---
+++ custom_lodash/find.js
@@ -0,0 +1,16 @@
+function find(array, predicate, fromIndex = 0) {
+  if (!predicate) return undefined
+  for (let i = 0; i < array.length; i += 1) {
+    if (!Array.isArray(array[i])) {
+      if (predicate(array[i])) {
+        return array[i];
+      }
+    }
+    for (let j = 0; j < array[i].length; j += 1) {
+      if (predicate(array[i][j], j, array[i])) {
+        return array[i][j];
+      }
+    }
+  }
+}
+export default find;
