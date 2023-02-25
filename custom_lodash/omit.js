e6d6441a1a881d6707aa3e3fff7913080441f28f
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-02-25 21:33:47 +0300

    feat: add implement func for test omit

--- custom_lodash/omit.js
+++ custom_lodash/omit.js
@@ -1,5 +1,21 @@
-function omit(){
+function omit(obj, paths) {
+  /*   test('omit property paths ["a", "c"] from object' , () => {
+      var object = { 'a': 1, 'b': '2', 'c': 3 };
+      expect(omit(object, ['a', 'c'])).toStrictEqual({'b': '2'});
+    }); */
+  let omittedObj = obj;
+  for (let key in obj) {
+    for (let i = 0; i < paths.length; i++) {
+      if (paths[i] === key) {
+        omittedObj[key] === obj[key]
+        delete omittedObj[key]
+      }
+    }
 
+
+  }
+  console.log(omittedObj)
+  return omittedObj
 }
 
 export default omit;
