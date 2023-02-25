d4e83af99087ccdc5d3e5bcb369fefe2da5d11d8
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-02-25 15:31:59 +0300

    feat: add function zip with for loop

--- custom_lodash/zip.js
+++ custom_lodash/zip.js
@@ -1,5 +1,11 @@
-function zip(arr) {
-
+function zip(...arr) {
+  let zippedArr = [];
+  let arrFirstElements = [];
+  let arrSecondElements = [];
+  for(let i = 0; i < arr.length; i++) {
+    arrFirstElements = [...arrFirstElements, arr[i][0]];
+    arrSecondElements = [...arrSecondElements,arr[i][1]];
+  }
+  return [...zippedArr, arrFirstElements, arrSecondElements];
 }
-
 export default zip;
