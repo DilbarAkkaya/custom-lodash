fe79a0f4a89019fe04b16a2418522f14b79e706c
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-02-27 17:04:47 +0300

    feat: implement func topairs

--- custom_lodash/index.test.js
+++ custom_lodash/index.test.js
@@ -233,7 +233,6 @@ describe("Filter Function", () => {
       this.a = 1;
       this.b = 2;
     }
-    Foo.prototype.c = 3;
     expect(toPairs(new Foo)).toStrictEqual([['a', 1], ['b', 2]]);
   });
 });
--- custom_lodash/toPairs.js
+++ custom_lodash/toPairs.js
@@ -1,5 +1,16 @@
-function toPairs(){
-
+function toPairs(obj){
+  let keyValueArr = [];
+  let keysArr = [];
+  for (let key in obj) {
+    keysArr = [...keysArr, key];
+  }  
+   console.log(keysArr)
+     for (let i = 0; i < keysArr.length; i++) {
+     keyValueArr = [...keyValueArr, [keysArr[i], obj[keysArr[i]]]]
+   }
+  return keyValueArr;
 }
+//[['a', 1], ['b', 2]]
+console.log(toPairs({ 'a': 1, 'c': 3 }))
 
 export default toPairs;
