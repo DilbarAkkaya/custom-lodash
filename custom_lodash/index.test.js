be197e53e66e6485a0cce09edb61c070eaca6641
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-02-27 14:58:48 +0300

    feat: define func onPairs

--- custom_lodash/index.test.js
+++ custom_lodash/index.test.js
@@ -13,6 +13,7 @@ import omit from './omit.js';
 import omitBy from './omitBy.js';
 import pick from './pick.js';
 import pickBy from './pickBy.js';
+import onPairs from './onPairs.js';
 
 describe("Filter Function", () => {
   test("function Filter is defined", () => {
@@ -221,7 +222,7 @@ describe("Filter Function", () => {
     var object = { 'a': 1, 'b': '2', 'c': 3 };
     expect(pickBy(object, (key) => typeof key === 'number')).toStrictEqual({ 'a': 1, 'c': 3 });
   });
-  
+
   test("function onPairs is defined", () => {
     expect(onPairs).toBeDefined();
   });
---
+++ custom_lodash/onPairs.js
@@ -0,0 +1,5 @@
+function onPairs(){
+
+}
+
+export default onPairs;
