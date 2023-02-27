abb16ac96fe6bf579f8e41fea10c3f58762d8b47
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-02-27 14:44:29 +0300

    feat: implement pickedby func

--- custom_lodash/pickBy.js
+++ custom_lodash/pickBy.js
@@ -1,5 +1,11 @@
-function pickBy() {
-
+function pickBy(obj, predicate) {
+  let pickedByObj = {};
+  for (let key in obj) {
+    if (predicate(obj[key])) {
+      pickedByObj = { ...pickedByObj, [key]: obj[key] }
+    }
+  }
+  return pickedByObj
 }
 
 export default pickBy;
